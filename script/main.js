28a5d4a0003d3fa4145e04fda14b5d3fc199ff69
rachidAfggan <afggan.rachid@gmail.com>
2022-10-08 01:34:56 +0100

    Exempl Portfolio Wakanda Landing Page

---
+++ script/main.js
@@ -0,0 +1,15 @@
+let openBtn = document.querySelector(".fa-bars");
+let closeBtn = document.querySelector(".fa-xmark");
+let menu =document.querySelector(".navbar__menu")
+
+openBtn.addEventListener('click',()=>{
+    openBtn.classList.toggle("hide");
+    closeBtn.classList.toggle("hide");
+    menu.classList.toggle("hidden");
+});
+
+closeBtn.addEventListener('click',()=>{
+    openBtn.classList.toggle("hide");
+    closeBtn.classList.toggle("hide");
+    menu.classList.toggle("hidden");
+});
