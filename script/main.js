e87417cc276d6200fee181821311723a9b4c0eec
rachidAfggan <afggan.rachid@gmail.com>
2022-10-15 02:25:40 +0100

    Add footer section & top button & animation

--- script/main.js
+++ script/main.js
@@ -1,6 +1,7 @@
 let openBtn = document.querySelector(".fa-bars");
 let closeBtn = document.querySelector(".fa-xmark");
 let menu =document.querySelector(".navbar__menu")
+let scroll= document.querySelector(".fa-regular");
 
 openBtn.addEventListener('click',()=>{
     openBtn.classList.toggle("hide");
@@ -13,3 +14,16 @@ closeBtn.addEventListener('click',()=>{
     closeBtn.classList.toggle("hide");
     menu.classList.toggle("hidden");
 });
+
+//Scroll
+
+window.onscroll = ()=>{
+    this.scrollY >= 1000 ?  scroll.classList.add("show") : scroll.classList.remove("show") 
+};
+
+scroll.onclick = ()=>{
+    window.scrollTo({
+        top:0,
+        behavior: "smooth",
+    })
+}
